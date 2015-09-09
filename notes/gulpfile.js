f7ca7e52a2979379c3259e3dbf6858ce116155e7
Esbjörn Blomquist <esbblo@gmail.com>
2015-09-09 05:51:37 +0200

    gulpfile.js, work in progress
    Signed-off-by: Esbjörn Blomquist <esbblo@gmail.com>

--- notes/gulpfile.js
+++ notes/gulpfile.js
@@ -9,6 +9,13 @@ var sass = require('gulp-sass');
 var gls = require('gulp-live-server');
 var del = require('del');
 var typescript = require('gulp-typescript');
+var merge = require('merge2');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var reactify = require('reactify');
+// var uglify = require('gulp-uglify');
 // var tslint = require('gulp-tslint');
 // var Karma = require('karma').Server;
 
@@ -17,11 +24,6 @@ var typescript = require('gulp-typescript');
 var config = {
     libs: {
         js: [
-            'node_modules/angular/angular.min.js',
-            'node_modules/angular-route/angular-route.min.js',
-            'bower_components/angular-aria/angular-aria.min.js',
-            'bower_components/angular-animate/angular-animate.min.js',
-            'bower_components/angular-material/angular-material.min.js',
             'lib/colors.js',
         ],
         style: [
@@ -29,70 +31,64 @@ var config = {
             'bower_components/mdi/!(scss)*/*',
         ],
         map: [
-            'node_modules/angular/angular.min.js.map',
             'bower_components/mdi/css/materialdesignicons.min.css.map',
-            'bower_components/angular-animate/angular-animate.min.js.map',
-            'bower_components/angular-aria/angular-aria.min.js.map',
-            'node_modules/angular-route/angular-route.min.js.map',
         ]
     },
-    test: {
-        conf: '\src\test\karma-conf.js',
+    ts: {
+        src: 'src/app/**/*.ts',
+        conf: {
+            module: 'commonjs',
+            target: 'ES5',
+            sortOutput: true
+        },
     },
-    app: {
+    src: {
         root: 'src/app',
-        ts: ['src/app/**/*.module.ts', 'src/app/**/*.service.ts', 'src/app/**/*.controller.ts'],
-        js: 'src/app/tsout.js',
-        parts: 'src/app/parts/*.html',
-        img: 'src/app/img/*',
+        entry: './src/app/entry.js',
+        js: 'src/app/**/*.js',
+        parts: 'src/app/**/*.html',
+        img: 'src/img/*',
         index: 'src/index.html',
         style: 'src/style/main.scss',
         styles: 'src/style/**/*',
+        toClean: 'src/app/**/!(entry)*.js'
     },
     dist: {
         root: 'dist',
         files: 'dist/**/*',
         parts: 'dist/parts',
         img: 'dist/img',
+        jsAppName: 'app.js',
+        jsLibName: 'lib.js',
+        browserifyBundle: 'bundle.js',
     },
     server: {
         lrPort: 35729,
         port: 5000
     },
-    tsConf: {
-        module: 'commonjs',
-        target: 'ES5',
-        out: 'tsout.js'
-    }
+    test: {
+        conf: '\src\test\karma-conf.js',
+    },
 };
 
 /////////// COMPILE AND MOVING TASKS ///////////
 
-gulp.task('clean', function(cb) {  // bugs?
-    del([config.dist.files], cb);
+gulp.task('clean', function() {  // bugs?
+    del([config.dist.files]);
+    del([config.src.toClean]);
 });
 
-gulp.task('appCompile', function() {
-    gulp.src(config.app.ts)
-        // .pipe(tslint)
-        // .pipe(tslint.report, 'verbose')
-        .pipe(typescript(config.tsConf))
-        .pipe(gulp.dest(config.app.root));
-});
-        
-gulp.task('appJs', function() {
-    gulp.src(config.app.js)
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest(config.dist.root));
+gulp.task('ts', function() {
+    var tsResult = gulp.src(config.ts.src)
+        .pipe(typescript(config.ts.conf));
+    
+    return merge([
+        tsResult.js
+            .pipe(gulp.dest(config.src.root))
+    ]);
 });
 
-gulp.task('app', ['appCompile'], function() {  // Same as tspcomJs, but with dependency to ts compile task
-    gulp.src(config.app.js)
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest(config.dist.root));
-});
-
-gulp.task('html', function() {
+gulp.task('content', function() {
     gulp.src(config.app.index)
         .pipe(gulp.dest(config.dist.root));
     gulp.src(config.app.parts)
@@ -103,7 +99,7 @@ gulp.task('html', function() {
 
 gulp.task('libs', function() {
     gulp.src(config.libs.js)
-        .pipe(concat('lib.js'))
+        .pipe(concat(config.dist.jsLibName))
         .pipe(gulp.dest(config.dist.root));
     gulp.src(config.libs.map)
         .pipe(gulp.dest(config.dist.root));
@@ -118,7 +114,26 @@ gulp.task('style', function() {
         .pipe(gulp.dest(config.dist.root));
 });
 
-gulp.task('build', ['app', 'html', 'libs', 'style']);  //'clean',
+gulp.task('browserify', function () {
+    var b = browserify({
+        entries: config.src.entry,
+        debug: true,
+        transform: [reactify], // defining transforms here will avoid crashing your stream
+        ignoreMissing: true // For ignoring error messages. The websocket lib is using require as an argument for a function.
+    });
+    
+    return b.bundle()
+        .pipe(source(config.dist.browserifyBundle))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+            // Add transformation tasks to the pipeline here.
+            // .pipe(uglify())
+            // .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.dist.root));
+});
+
+gulp.task('build', ['ts', 'libs', 'content', 'style', 'browserify']);  //'clean',
 
 /////////// SERVER AND WATCH TASKS ///////////
 
@@ -131,17 +146,12 @@ gulp.task('server', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(config.app.ts, ['appCompile']);
-    gulp.watch(config.app.js, ['appJs']);
-    gulp.watch(config.app.styles, ['style']);
-    gulp.watch(config.app.index, ['html']);
-    gulp.watch(config.app.parts, ['html']);
-    gulp.watch(config.app.img, ['html']);
-    gulp.watch(config.app.js, ['app']);
-});
-
-gulp.task('tsWatch', function() {
-    gulp.watch(config.app.ts, ['appCompile']);
+    gulp.watch(config.ts.src, ['app']);
+    gulp.watch(config.src.js, ['browserify']);
+    gulp.watch(config.src.styles, ['style']);
+    gulp.watch(config.src.index, ['content']);
+    gulp.watch(config.src.parts, ['content']);
+    gulp.watch(config.src.img, ['content']);
 });
 
 /////////// TESTING TASKS //////////////
