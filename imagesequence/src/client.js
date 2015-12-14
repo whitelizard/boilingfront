6ad1027f66de04e2d8f9e8d064f4877e9b56429e
Esbjörn Blomquist <esbblo@gmail.com>
2015-12-14 09:53:34 +0100

    wip

---
+++ imagesequence/src/client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Hello</div>,
+    document.body
+);
+
+//document.write('welcome to my app');
+
+//console.log('app loaded');
